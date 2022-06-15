8dcb724765b2da4f726b387570c9645902a89a17
rafiramadian <rafiramadian.r@upi.edu>
2022-06-15 19:46:54 +0700

    Modify user model to customer model

---
+++ kantoor_app/lib/models/customer.dart
@@ -0,0 +1,13 @@
+class Customer {
+  String username;
+  String email;
+  String alamat;
+  String password;
+
+  Customer({
+    required this.username,
+    required this.email,
+    required this.alamat,
+    required this.password,
+  });
+}
