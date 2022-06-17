b5371d3f4b9657d99bd23e8e77af19ebc1280cd1
thsanggiii <anggitaajeng307@gmail.com>
2022-06-17 19:49:05 +0700

    create jenis gedung model, message model

---
+++ kantoor_app/lib/models/jenis_gedung.dart
@@ -0,0 +1,11 @@
+class JenisGedung {
+  String id;
+  String idGedung;
+  String jenis;
+
+  JenisGedung({
+    required this.id,
+    required this.idGedung,
+    required this.jenis,
+  });
+}
---
+++ kantoor_app/lib/models/message.dart
@@ -0,0 +1,13 @@
+class Message {
+  String id;
+  String idCustomer;
+  String createdAt;
+  String text;
+
+  Message({
+    required this.id,
+    required this.idCustomer,
+    required this.createdAt,
+    required this.text,
+  });
+}
